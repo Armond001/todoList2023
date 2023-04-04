2140c9a619dd57075d084feb80e73e4bb09b4a85
Armond001 <Armond0621@gmail.com>
2023-04-03 19:21:56 -0500

    use state added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+/*
 import './App.css';
 import InstagramPost from"./instagramPost";
 
@@ -24,3 +24,44 @@ function App() {
 }
 
 export default App;
+*/
+
+
+
+import React from "react";
+
+
+
+
+function App(){
+  const[listItem,setListItem] = React.useState([]);
+  // this is basically 
+
+  /*
+  const usestate=()=>{
+    //logic goes here
+    return[yourVariable,setYourVariable]
+  }
+  */
+
+  return(
+    <div>
+      <ul>
+        {
+          listItem.map((item,index) => {
+            return(<li key={index}>{item}</li>)
+            
+          })
+        }
+
+      </ul>
+      <input 
+        onChange={(event)=> 
+        setListItem(event.target.value)
+        }
+        />
+    </div>
+  );
+};
+
+export default App
